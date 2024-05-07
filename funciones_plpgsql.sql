f7cb8d4f25b26cdbf4a7e6331b972a9f8c5ab260
AntonioTNDR <119497606+AntonioTNDR@users.noreply.github.com>
2024-05-07 10:56:39 +0200

    Update funciones_plpgsql.sql

--- funciones_plpgsql.sql
+++ funciones_plpgsql.sql
@@ -1,1 +1,7 @@
 --AQUÍ IRÍAN LAS FUNCIONES--
+--ENUNCIOADO--
+--El frente popular de Judea quiere ganar votos para las proximas elecciones. Nos ha encargado que en 
+--LUEGO PONEMOS EL LORE--
+--El programa actualiza los saldos de los clientes dependiendo si son personas (diferenciando los >65 años) u organizaciones (PYMES, grandes empresas).--
+--Dependiendo de su condición, se actulaizan los saldos correspondeintemente.--
+--La función devolvería varios registros indicando el código de cliente, su sucursal, su saldo antes y después del cambio y el tipo de persona u organización.--
