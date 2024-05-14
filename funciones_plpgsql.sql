8d1e553cc09764980ee06e0a43cde7de18e9c955
ByDageta <119523917+ByDageta@users.noreply.github.com>
2024-05-14 12:43:06 +0200

    Update funciones_plpgsql.sql

--- funciones_plpgsql.sql
+++ funciones_plpgsql.sql
@@ -9,10 +9,11 @@ SCRIPT DE CREACIÓN DE FUNCIONES ""TENTATIVAS"" DE LA BASE DE DATOS DEL BANCO
 
 
 --=====================================
---				control
---				FUNCIONA, NO TOCAR
+--              control
+--         FUNCIONA, NO TOCAR
 --=====================================
-CREATE OR REPLACE FUNCTION control() RETURNS TABLE(sucursal SUCURSAL.su_id%TYPE, cliente CUENTA.codigo%TYPE, tipo_cliente varchar(20), saldo_anterior CUENTA.saldo_actual%TYPE, saldo_nuevo CUENTA.saldo_actual%TYPE) AS $$ 
+
+CREATE OR REPLACE FUNCTION control_anual() RETURNS TABLE(sucursal SUCURSAL.su_id%TYPE, cliente CUENTA.codigo%TYPE, tipo_cliente varchar(20), saldo_anterior CUENTA.saldo_actual%TYPE, saldo_nuevo CUENTA.saldo_actual%TYPE) AS $$ 
 DECLARE
 	curs_suc CURSOR FOR SELECT su_id FROM SUCURSAL;
 	curs_cli CURSOR(su_par SUCURSAL.su_id%TYPE) FOR SELECT * FROM CUENTA WHERE su_id = su_par;
@@ -22,71 +23,81 @@ DECLARE
 BEGIN
 	FOR rec1 IN curs_suc LOOP
 		FOR rec2 IN curs_cli(rec1.su_id) LOOP
-			subtipo := checkSubtipo(rec2.codigo);
+			subtipo := check_subtipo(rec2.codigo);
 			saldo_old := (SELECT rec2.saldo_actual);
 			saldo_new := actualizar_saldo(curs_cli, subtipo);
 			RETURN QUERY SELECT rec1.su_id, rec2.codigo, subtipo, saldo_old, saldo_new;
 		END LOOP;
 	END LOOP;
 	RETURN;
-END $$ LANGUAGE plpgsql;
+EXCEPTION
+    WHEN OTHERS THEN RAISE NOTICE 'Ha ocurrido un error inesperado.';
+END
+$$ LANGUAGE plpgsql;
+
 
 --=====================================
---				checkSubtipo
---				NO TOCAR, FUNCIONA
+--           check_subtipo
+--         NO TOCAR, FUNCIONA
 --=====================================
-DROP FUNCTION checkSubtipo;
-CREATE OR REPLACE FUNCTION checkSubtipo(codc_arg CLIENTE.codigo%TYPE) RETURNS varchar AS $$
+
+DROP FUNCTION check_subtipo;
+CREATE OR REPLACE FUNCTION check_subtipo(codc CLIENTE.codigo%TYPE) RETURNS varchar AS $$
 DECLARE 
-mycod CLIENTE.codigo%TYPE;
-fecha_act date:=(SELECT CURRENT_DATE);
-fecha_nac date;
-param varchar(20);
+    cod CLIENTE.codigo%TYPE;
+    fecha_now DATE := (SELECT CURRENT_DATE);
+    fecha_born DATE;
+    subtipo varchar(20);
 BEGIN 
-	SELECT codigo INTO mycod FROM ORGANIZACION WHERE codigo=codc_arg;
-	IF NOT FOUND THEN 
-		SELECT codigo,fecha_nacimiento INTO STRICT mycod,fecha_nac FROM PERSONA WHERE codigo=codc_arg;
-		IF fecha_act-fecha_nac>23742 --65 años en numero de dias(contando años bisiestos)--
-		THEN param:='p_mayor';
-		ELSE param:='p_menor';
-		END IF; 
-
-	ELSE SELECT tipo INTO param FROM ORGANIZACION WHERE codigo=mycod;
-	END IF; 
-RETURN param;
+	SELECT codigo INTO cod FROM ORGANIZACION WHERE codigo = codc;
+	IF NOT FOUND THEN
+		SELECT codigo,fecha_nacimiento INTO STRICT cod,fecha_born FROM PERSONA WHERE codigo = codc;
+		IF fecha_now - fecha_born > 23742 -- Comrpobar si tiene más de 65 años (en numero de dias, contando años bisiestos)
+		    THEN subtipo := 'p_mayor';
+		    ELSE subtipo := 'p_menor';
+		END IF;
+	ELSE SELECT tipo INTO subtipo FROM ORGANIZACION WHERE codigo = cod;
+	END IF;
+    RETURN subtipo;
 EXCEPTION 
-	WHEN NO_DATA_FOUND THEN RAISE NOTICE 'El cliente no existe en la base de datos';
+	WHEN NO_DATA_FOUND THEN RAISE NOTICE 'El cliente no está registrado en la Base de Datos.';
+    WHEN TOO_MANY_ROWS THEN RAISE NOTICE 'FATAL ERROR: Hay mas de un cliente con el mismo codigo.';
+    WHEN OTHERS THEN RAISE NOTICE 'Ha ocurrido un error inesperado.';
 END 
 $$ LANGUAGE plpgsql;
 
+
 --=====================================
---				actualizar_saldo
---				FUNCIONA, NO TOCAR
+--           actualizar_saldo
+--          FUNCIONA, NO TOCAR
 --=====================================
+
 -- Actualizar el saldo dependiendo del tipo (y subtipo) del cliente en cuestión
 CREATE OR REPLACE FUNCTION actualizar_saldo(curs REFCURSOR, subtipo varchar(20), saldo_new OUT CUENTA.saldo_actual%type) AS $$
 BEGIN
     CASE subtipo
         WHEN 'p_menor' THEN
             RAISE NOTICE 'Persona Menor de 65 años';
-            UPDATE CUENTA SET saldo_actual = saldo_actual*0.9
+            UPDATE CUENTA SET saldo_actual = saldo_actual * 0.9
             WHERE CURRENT OF curs
             RETURNING saldo_actual INTO saldo_new;  -- Actualizar el saldo del cliente (-10%)
         WHEN 'p_mayor' THEN
             RAISE NOTICE 'Persona Mayor de 65 años';
-            UPDATE CUENTA SET saldo_actual = saldo_actual*1.25
+            UPDATE CUENTA SET saldo_actual = saldo_actual * 1.1
             WHERE CURRENT OF curs
-            RETURNING saldo_actual INTO saldo_new;  -- Actualizar el saldo del cliente (+25%)
+            RETURNING saldo_actual INTO saldo_new;  -- Actualizar el saldo del cliente (+10%)
         WHEN 'PYME' THEN
             RAISE NOTICE 'Organización PYME';
-            UPDATE CUENTA SET saldo_actual = saldo_actual*0.85
+            UPDATE CUENTA SET saldo_actual = saldo_actual * 0.93
             WHERE CURRENT OF curs
-            RETURNING saldo_actual INTO saldo_new;  -- Actualizar el saldo del cliente (-15%)
+            RETURNING saldo_actual INTO saldo_new;  -- Actualizar el saldo del cliente (-7%)
         WHEN 'Gran empresa' THEN
             RAISE NOTICE 'Organización Gran Empresa';
-            UPDATE CUENTA SET saldo_actual = saldo_actual*0.95
+            UPDATE CUENTA SET saldo_actual = saldo_actual * 0.95
             WHERE CURRENT OF curs
             RETURNING saldo_actual INTO saldo_new;  -- Actualizar el saldo del cliente (-5%)
     END CASE;
+EXCEPTION
+    WHEN OTHERS THEN RAISE NOTICE 'Ha ocurrido un error inesperado.';
 END
 $$ LANGUAGE plpgsql;
